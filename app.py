0908307f6c6f0fd4db412d87ed34c739eec5ac4d
Antony Collett <Antony.Collett@oneadvanced.com>
2024-08-05 14:41:00 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,5 @@
+# Book recommendation application.
+# Choose fiction or non-fiction. These will be separate libraries.
+# Choose a genre.
+# Choose length of book.
+# Choose if book is standalone or part of a series.
