5510a586e9157f720a1cbb6974e92da87f343eb7
Antony Collett <Antony.Collett@oneadvanced.com>
2024-08-14 16:24:45 +0100

    created node class and imported into app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 # Book recommendation application.
-# Choose fiction or non-fiction. These will be separate libraries.
-# Choose a genre.
-# Choose length of book.
-# Choose if book is standalone or part of a series.
+# Choose fiction or non-fiction. These will be separate libraries. - Nice to have
+# Choose a genre. - Must have
+# Choose length of book. - Nice to have
+# Choose if book is standalone or part of a series - Nice to have
+
+from node import *
+
+
+my_node = node("Antony")
+
+print(my_node.get_value())
