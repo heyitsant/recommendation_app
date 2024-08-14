c110b0b157933f165d6875db2fc773b1e4e9bd8a
Antony Collett <Antony.Collett@oneadvanced.com>
2024-08-14 17:26:52 +0100

    added import and function call to the end of find_string.py for testing

--- find_string.py
+++ find_string.py
@@ -1,10 +1,15 @@
+import csv
+
 def find_string(input, column_number):
     # Function to find and separate words in long string.
     # This function will return a dictionary of unique terms found in specified column, as a Python dictionary
-
-    with open('input', encoding="utf-8") as csv_file:
+    
+    with open(input, encoding="utf-8") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ',')
         genres = []
         word = ""
         for row in csv_reader:
             print(row[column_number])
+
+
+find_string('goodreads_data.csv', 3)
