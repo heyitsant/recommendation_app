e2d525fa5c92596aa8d8d7b57a4a09703471f1e6
Antony Collett <Antony.Collett@oneadvanced.com>
2024-09-06 17:32:40 +0100

    Began working on the main loop to retrieve books with the matching genre

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ def book_data():
 my_book_genres = genre_list()
 my_book_list = book_data()
 
+#print(my_book_genres.stringify_list())
+
 selected_genre = ""
 
 while len(selected_genre) == 0:
@@ -40,9 +42,22 @@ while len(selected_genre) == 0:
     matching_types = []
     type_list_head = my_book_genres.get_head_node()
     while type_list_head is not None:
-        if str(type_list_head.get_value()).startswith(user_input):
-            matching_types.append(type_list_head.get_value())
+        if str(type_list_head.get_value()).lower().startswith(user_input):
+            if str(type_list_head.get_value()) not in matching_types:
+                matching_types.append(type_list_head.get_value())
         type_list_head = type_list_head.get_next_node()
-
+    print("\n")
     for genre in matching_types:
         print(genre)
+
+    if len(matching_types) == 1:
+        select_genre = str(input("\n" + matching_types[0] +" is the only matching genre for your choice.\nDo you want to see the books that match this genre?\nEnter y for yes or n for no\n")).lower()
+
+        if select_genre == "y":
+            selected_genre = matching_types[0]
+            print("Selected genre: " + selected_genre)
+            book_list_head = my_book_list.get_head_node()
+            while book_list_head.get_next_node() is not None:
+                sublist_head = book_list_head.get_value().get_head_node()
+                print(sublist_head.value)
+
